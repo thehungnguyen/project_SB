4e58cb3028afbab12c2446a8bba15edd620fd512
thehungnguyen <nguyenthehung22122002@gmail.com>
2024-06-26 14:58:08 +0700

    Exception Handling and Validation

--- src/main/java/com/hungnt/project_SB/dto/request/UserCreateReq.java
+++ src/main/java/com/hungnt/project_SB/dto/request/UserCreateReq.java
@@ -1,9 +1,14 @@
 package com.hungnt.project_SB.dto.request;
 
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
 public class UserCreateReq {
+    @Size(min = 5, message = "Username must be at least 5 characters")
     private String username;
+
+    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
     private String firstName;
     private String lastName;
